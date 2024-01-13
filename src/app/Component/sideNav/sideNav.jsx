9d3ebd4fd1a70c8fe3d2da012f9e016679ddb646
Sudheep24 <sudheeps2004@gmail.com>
2024-01-13 13:18:09 +0530

    f

--- src/app/Component/sideNav/sideNav.jsx
+++ src/app/Component/sideNav/sideNav.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './sidenav.css'
+import Search from '../Search/Search'
 
 export default function SideNav() {
   return (
     <div> 
         <div className="sidenav">
-            jdj
+          <div className="sear">
+          <select id="dropdown" name="dropdown">
+            <option value="option1">Cricket</option>
+            <option value="option2">Tennis</option>
+            <option value="option3">Football</option>
+        </select>
+            </div>
         </div>
         
         sideNav
