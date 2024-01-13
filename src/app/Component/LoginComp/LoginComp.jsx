3b42cdc253fba3d0aeb32983081afa4d1989a769
Sudheep24 <sudheeps2004@gmail.com>
2024-01-13 18:22:21 +0530

    login

--- src/app/Component/LoginComp/LoginComp.jsx
+++ src/app/Component/LoginComp/LoginComp.jsx
@@ -17,7 +17,7 @@ export default function LoginComp() {
         <input type="text" placeholder='password' className='pass' />
 
 
-        <span className='sss'>If you don't have an account? </span>
+        <span className='sss'>If you dont have an account? <a href='/signup'>Signup</a></span>
         <button className='logbut'> Login</button>
         </div>
         
