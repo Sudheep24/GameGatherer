9f11ae9a00d7163a0b1a58e87735441b5a3c8b0b
Sudheep24 <sudheeps2004@gmail.com>
2024-01-13 16:24:45 +0530

    hjdhf

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -4,25 +4,7 @@ import './login.css';
 export default function Page() {
   return (
     <div className='fo'>
-      <div className="form-container">
-        <p className="title">Welcome back</p>
-        <form className="form">
-          <div className="input-container">
-            <input type="email" className="input" placeholder="Email" />
-          </div>
-          <div className="input-container">
-            <input type="password" className="input" placeholder="Password" />
-          </div>
-          <p className="page-link">
-            <span className="page-link-label">Forgot Password?</span>
-          </p>
-          <button className="form-btn">Log in</button>
-        </form>
-        <div className="sign-up-label">
-          Don't have an account?<span className="sign-up-link"><a href='/signup'>Sign up</a></span>
-        </div>
-        
-      </div>
+      
     </div>
   );
 }
