a000cb6753986cd2d55218ec8812f241647ead1f
Sudheep24 <sudheeps2004@gmail.com>
2024-01-13 18:18:27 +0530

    login

---
+++ src/app/Component/LoginComp/LoginComp.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function LoginComp() {
+  return (
+    <div className='logg'>
+        <div>
+        <span className='lo'>Login</span>
+        <div>
+        <span className='username'>
+            Username
+        </span>
+        <input type="text" placeholder='username' className='user' />
+        </div>
+        <span className='password'>
+            Password
+        </span>
+        <input type="text" placeholder='password' className='pass' />
+
+
+        <span className='sss'>If you don't have an account? <a href='/signup'>Signup</a></span>
+        <button className='logbut'> Login</button>
+        </div>
+        
+    </div>
+  )
+}
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,27 +1,11 @@
 import React from 'react'
 import './login.css'
+import LoginComp from '../Component/LoginComp/LoginComp'
 
 export default function page() {
   return (
-    <div className='logg'>
-        <div>
-        <span className='lo'>Login</span>
-        <div>
-        <span className='username'>
-            Username
-        </span>
-        <input type="text" placeholder='username' className='user' />
-        </div>
-        <span className='password'>
-            Password
-        </span>
-        <input type="text" placeholder='password' className='pass' />
-
-
-        <span className='sss'>If you don't have an account? <a href='/signup'>Signup</a></span>
-        <button className='logbut'> Login</button>
-        </div>
-        
-    </div>
+    <>
+        <LoginComp></LoginComp>
+    </>
   )
 }
