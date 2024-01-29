b51a2608ed5ce1c5794c37a88d1ba79245f40c22
Sudheep24 <sudheeps2004@gmail.com>
2024-01-29 21:36:04 +0530

    login

--- src/app/Component/About/About.jsx
+++ src/app/Component/About/About.jsx
@@ -9,7 +9,7 @@ export default function Section2() {
 
       <span className='moreti'>Search.Find.Play</span>
 
-      <p>GameGatherer is your ultimate destination for all things gaming. As an avid gamers haven, GameGatherer provides a bustling online hub where players unite to explore, compete, and celebrate their passion for gaming. Dive into a rich tapestry of diverse game genres, from adrenaline-pumping action to mind-bending puzzles and immersive storytelling adventures. Engage in vibrant communities, exchange tips and strategies, and forge lasting friendships with fellow gamers. With a seamless and intuitive platform, GameGatherer offers an unparalleled gaming experience tailored to every player's preferences and playstyle. Join the gathering, and let the games begin!</p>
+      <p>GameGatherer is your ultimate destination for all things gaming. As an avid gamers haven, GameGatherer provides a bustling online hub where players unite to explore, compete, and celebrate their passion for gaming. Dive into a rich tapestry of diverse game genres, from adrenaline-pumping action to mind-bending puzzles and immersive storytelling adventures. Engage in vibrant communities, exchange tips and strategies, and forge lasting friendships with fellow gamers. With a seamless and intuitive platform, GameGatherer offers an unparalleled gaming experience tailored to every players preferences and playstyle. Join the gathering, and let the games begin!</p>
     </div>
   )
 }
