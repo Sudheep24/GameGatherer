660ffd3c41bd9eb7b0e56fbca0182539caec1a9d
Sudheep24 <sudheeps2004@gmail.com>
2024-01-30 21:50:07 +0530

    frontend

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,9 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-      <script src="https://kit.fontawesome.com/56f8f15525.js" crossorigin="anonymous"></script>
-      </Head>
+      
       <body className={inter.className}>
         <AuthProvider>
         <Navbar></Navbar>
